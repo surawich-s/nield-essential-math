97ef7dfc6276d10f51b2d250eb296456bf275117
surawich-s <surawich.sit@gmail.com>
2025-01-10 23:24:24 +0700

    derivative

--- Chapter 1/derivatives-1-18,19.py
+++ Chapter 1/derivatives-1-18,19.py
@@ -2,11 +2,16 @@ from sympy import *
 # Declare 'x' to SymPy
 x = symbols('x')
 # Now just use Python syntax to declare function
-f = 3*x**2+1
+f = (3*x**2 + 2*x - 5) / (x**2 - 4*x + 3)
 # Calculate the derivative of the function
 dx_f = diff(f)
 
 print(dx_f)
+
+# Simplify the derivative expression
+simplified_dx_f = simplify(dx_f)
+print("Simplified derivative:")
+print(simplified_dx_f)
 
 # def f(x):
 #     return 3*x**2+1
@@ -16,7 +21,7 @@ print(dx_f)
 # slope_at_2 = dx_f(3)
 # print(slope_at_2)
 
-dx_f_callable = lambdify(x, dx_f)
-result2 = dx_f_callable(2) 
+# dx_f_callable = lambdify(x, dx_f)
+# result2 = dx_f_callable(2) 
 
-print(result2)
+# print(result2)
